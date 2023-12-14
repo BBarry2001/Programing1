3bcfd47325dd1b876a96c3110b43f35c2aa60b3b
Boubacar Barry <bboubacar98@gmail.com>
2023-12-13 18:45:02 -0600

    Module 5

--- WeeklyTemperatures.java
+++ WeeklyTemperatures.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class WeeklyTemperatures { 
+public class WeeklyTemperatures {  
     public static void main(String[] args) {
         ArrayList<String> daysOfWeek = new ArrayList<>();
         ArrayList<Double> temperatures = new ArrayList<>(); 
